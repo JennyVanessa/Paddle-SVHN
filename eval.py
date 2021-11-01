d1a2199e66561676bb299ed303b94e4b2c5c6435
tsdlrh <2541002015@qq.com>
2021-11-01 23:14:06 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -11,13 +11,13 @@ parser.add_argument('checkpoint', type=str, help='path to evaluate checkpoint, e
 def _eval(path_to_checkpoint_file, path_to_eval_lmdb_dir):
     model = Model()
     model.restore(path_to_checkpoint_file)
-    model.cuda()
+    # model.cuda()
     accuracy = Evaluator(path_to_eval_lmdb_dir).evaluate(model)
     print('Evaluate %s on %s, accuracy = %f' % (path_to_checkpoint_file, path_to_eval_lmdb_dir, accuracy))
 
 
 def main(args):
-    path_to_test_lmdb_dir = os.path.join(args.data_dir, 'test.lmdb')
+    path_to_test_lmdb_dir = os.path.join(args.data_dir, 'val.lmdb')
     path_to_checkpoint_file = args.checkpoint
 
     print('Start evaluating')
